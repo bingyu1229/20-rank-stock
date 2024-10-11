d989c0beb4872274722f12c852263391096b6e88
bingyu1229 <bingyu.he@yale.edu>
2024-10-11 10:44:24 +0800

    waitress

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def index():
     return render_template('index.html', stocks=None, current_time=None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
