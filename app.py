596e8896733aeb71ee34b6f4aec1e7a9704f2cc9
bingyu1229 <bingyu.he@yale.edu>
2024-10-11 10:54:20 +0800

    clean

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import pandas as pd
 import concurrent.futures
 from datetime import datetime
